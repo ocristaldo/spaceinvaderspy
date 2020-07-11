21e3204b541824057fe6d3f78c862b71e013fe61
Cristaldo, Omar <Omar.Cristaldo@tabcorp.com.au>
2020-07-11 18:31:21 +1000

    adding alien class and alien test

---
+++ aliens.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+
+#-----------------------------------
+# classes
+#-----------------------------------
+
+class Alien(pygame.sprite.Sprite):
+    """
+    This class represents the aliens.
+    It derives from the "Sprite" class in Pygame.
+    """
+
+    # set up the colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+
+    def __init__(self, color, width, height):
+        """
+        Ellipse Constructor. Pass in the color of the ellipse,
+        and its size
+        """
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+    
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+    
+        # Draw the ellipse
+        pygame.draw.ellipse(self.image, color, [0, 0, width, height])
